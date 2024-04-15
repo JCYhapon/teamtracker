df3e293a2d130a9579817b2a97e792e01dc68b7c
JCYhapon <yhaponjc@gmail.com>
2024-04-15 15:45:29 +0800

    modified: removed team member finished layout

--- src/ContractTeamManagement.js
+++ src/ContractTeamManagement.js
@@ -368,19 +368,38 @@ const ContractTeamManagement = () => {
 
           {/* Deactivating Team Member */}
           <div className="w-[60vh] border p-4 rounded-md h-full">
-            <h2>Deactivate Team Member</h2>
             <form onSubmit={handleDeactivateTeamMember}>
-              <label>
-                Team Member Address:
-                <input
-                  type="text"
-                  name="teamMember"
-                  value={teamMemberAddress}
-                  onChange={(e) => setTeamMemberAddress(e.target.value)}
-                />
-              </label>
-              <br />
-              <button type="submit">Deactivate Team Member</button>
+              <div className="mt-6">
+                <label className="flex justify-start items-center">
+                  Team Member Address:
+                  <input
+                    type="text"
+                    name="teamMember"
+                    value={teamMemberAddress}
+                    onChange={(e) => setTeamMemberAddress(e.target.value)}
+                    className="ml-2 border p-2 rounded w-[25rem]"
+                  />
+                </label>
+              </div>
+              <div className="mt-4 flex justify-end ">
+                <button
+                  type="submit"
+                  className="border px-[10px] py-[5px] text-sm rounded-md bg-orange-600 text-white flex gap-2 items-center"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="14"
+                    width="10.5"
+                    viewBox="0 0 384 512"
+                  >
+                    <path
+                      fill="#ffffff"
+                      d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+                    />
+                  </svg>
+                  Deactivate member
+                </button>
+              </div>
             </form>
           </div>
         </div>
