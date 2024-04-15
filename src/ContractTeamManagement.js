0c6e5531b5bd2706ee6eaba04e941f2e02b7fb6b
JCYhapon <yhaponjc@gmail.com>
2024-04-15 13:34:27 +0800

    added logo of metamask

--- src/ContractTeamManagement.js
+++ src/ContractTeamManagement.js
@@ -20,9 +20,8 @@ const ContractTeamManagement = () => {
   const [notification, setNotification] = useState(null);
   const [teamMemberAddress, setTeamMemberAddress] = useState("");
 
-
-   // Function to handle adding a team member
-   const handleAddTeamMember = async (e) => {
+  // Function to handle adding a team member
+  const handleAddTeamMember = async (e) => {
     e.preventDefault();
     try {
       await addTeamMember(walletAddress, name);
@@ -40,33 +39,33 @@ const ContractTeamManagement = () => {
   const handleAssignTask = async (e) => {
     e.preventDefault();
     if (!teamMemberAddress || !taskDescription) {
-        setNotification("Please enter both the team member address and task description.");
-        return;
+      setNotification(
+        "Please enter both the team member address and task description."
+      );
+      return;
     }
     const success = await assignTask(teamMemberAddress, taskDescription);
     if (success) {
-        setNotification("Task assigned successfully");
+      setNotification("Task assigned successfully");
     } else {
-        setNotification("Failed to assign task. Please try again.");
+      setNotification("Failed to assign task. Please try again.");
     }
-};
-
+  };
 
   // Function to handle completing a task
   const handleCompleteTask = async (e) => {
     e.preventDefault();
     if (!teamMemberAddress) {
-        setNotification("Please enter the team member address.");
-        return;
+      setNotification("Please enter the team member address.");
+      return;
     }
     const success = await completeTask(teamMemberAddress);
     if (success) {
-        setNotification("Task completed successfully");
+      setNotification("Task completed successfully");
     } else {
-        setNotification("Failed to complete task. Please try again.");
+      setNotification("Failed to complete task. Please try again.");
     }
-};
-
+  };
 
   // Function to handle deactivating a team member
   const handleDeactivateTeamMember = async (e) => {
@@ -86,28 +85,28 @@ const ContractTeamManagement = () => {
   const handleReleasePayment = async (e) => {
     e.preventDefault();
     if (!teamMemberAddress || !amount) {
-        setNotification("Please enter both team member address and amount.");
-        return;
+      setNotification("Please enter both team member address and amount.");
+      return;
     }
     const success = await releasePayment(teamMemberAddress, amount);
     if (success) {
-        setNotification("Payment released successfully");
+      setNotification("Payment released successfully");
     } else {
-        setNotification("Failed to release payment. Please try again.");
+      setNotification("Failed to release payment. Please try again.");
     }
-};
-
+  };
 
   // Function to fetch team members
   const fetchTeamMember = async (walletAddress) => {
     try {
-      const teamMember = await contractTeamManagement.methods.teamMembers(walletAddress).call();
-      setTeamMembers(prevMembers => [...prevMembers, teamMember]);
+      const teamMember = await contractTeamManagement.methods
+        .teamMembers(walletAddress)
+        .call();
+      setTeamMembers((prevMembers) => [...prevMembers, teamMember]);
     } catch (error) {
       console.error(`Error fetching team member: ${error}`);
     }
   };
-  
 
   // Fetch team members when component mounts
   useEffect(() => {
@@ -121,7 +120,7 @@ const ContractTeamManagement = () => {
     if (typeof window.ethereum !== "undefined") {
       try {
         const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts"
+          method: "eth_requestAccounts",
         });
         console.log("Connected to MetaMask:", accounts[0]);
         setConnectedAddress(accounts[0]);
@@ -140,43 +139,177 @@ const ContractTeamManagement = () => {
       {notification && <p>{notification}</p>}
       {/* <button onClick={handleFetchTeamMembers}>Fetch Team Members</button> */}
       <div className="row">
-        <div className="column">
+        <div className="column flex items center">
           <button className="connect-metamask" onClick={connectToMetaMask}>
             Connect to MetaMask
           </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="70"
+            height="40"
+            viewBox="0 0 212 189"
+            id="metamask"
+          >
+            <g fill="none" fill-rule="evenodd">
+              <polygon
+                fill="#CDBDB2"
+                points="60.75 173.25 88.313 180.563 88.313 171 90.563 168.75 106.313 168.75 106.313 180 106.313 187.875 89.438 187.875 68.625 178.875"
+              ></polygon>
+              <polygon
+                fill="#CDBDB2"
+                points="105.75 173.25 132.75 180.563 132.75 171 135 168.75 150.75 168.75 150.75 180 150.75 187.875 133.875 187.875 113.063 178.875"
+                transform="matrix(-1 0 0 1 256.5 0)"
+              ></polygon>
+              <polygon
+                fill="#393939"
+                points="90.563 152.438 88.313 171 91.125 168.75 120.375 168.75 123.75 171 121.5 152.438 117 149.625 94.5 150.188"
+              ></polygon>
+              <polygon
+                fill="#F89C35"
+                points="75.375 27 88.875 58.5 95.063 150.188 117 150.188 123.75 58.5 136.125 27"
+              ></polygon>
+              <polygon
+                fill="#F89D35"
+                points="16.313 96.188 .563 141.75 39.938 139.5 65.25 139.5 65.25 119.813 64.125 79.313 58.5 83.813"
+              ></polygon>
+              <polygon
+                fill="#D87C30"
+                points="46.125 101.25 92.25 102.375 87.188 126 65.25 120.375"
+              ></polygon>
+              <polygon
+                fill="#EA8D3A"
+                points="46.125 101.813 65.25 119.813 65.25 137.813"
+              ></polygon>
+              <polygon
+                fill="#F89D35"
+                points="65.25 120.375 87.75 126 95.063 150.188 90 153 65.25 138.375"
+              ></polygon>
+              <polygon
+                fill="#EB8F35"
+                points="65.25 138.375 60.75 173.25 90.563 152.438"
+              ></polygon>
+              <polygon
+                fill="#EA8E3A"
+                points="92.25 102.375 95.063 150.188 86.625 125.719"
+              ></polygon>
+              <polygon
+                fill="#D87C30"
+                points="39.375 138.938 65.25 138.375 60.75 173.25"
+              ></polygon>
+              <polygon
+                fill="#EB8F35"
+                points="12.938 188.438 60.75 173.25 39.375 138.938 .563 141.75"
+              ></polygon>
+              <polygon
+                fill="#E8821E"
+                points="88.875 58.5 64.688 78.75 46.125 101.25 92.25 102.938"
+              ></polygon>
+              <polygon
+                fill="#DFCEC3"
+                points="60.75 173.25 90.563 152.438 88.313 170.438 88.313 180.563 68.063 176.625"
+              ></polygon>
+              <polygon
+                fill="#DFCEC3"
+                points="121.5 173.25 150.75 152.438 148.5 170.438 148.5 180.563 128.25 176.625"
+                transform="matrix(-1 0 0 1 272.25 0)"
+              ></polygon>
+              <polygon
+                fill="#393939"
+                points="70.313 112.5 64.125 125.438 86.063 119.813"
+                transform="matrix(-1 0 0 1 150.188 0)"
+              ></polygon>
+              <polygon
+                fill="#E88F35"
+                points="12.375 .563 88.875 58.5 75.938 27"
+              ></polygon>
+              <path
+                fill="#8E5A30"
+                d="M12.3750002,0.562500008 L2.25000003,31.5000005 L7.87500012,65.250001 L3.93750006,67.500001 L9.56250014,72.5625 L5.06250008,76.5000011 L11.25,82.1250012 L7.31250011,85.5000013 L16.3125002,96.7500014 L58.5000009,83.8125012 C79.1250012,67.3125004 89.2500013,58.8750003 88.8750013,58.5000009 C88.5000013,58.1250009 63.0000009,38.8125006 12.3750002,0.562500008 Z"
+              ></path>
+              <g transform="matrix(-1 0 0 1 211.5 0)">
+                <polygon
+                  fill="#F89D35"
+                  points="16.313 96.188 .563 141.75 39.938 139.5 65.25 139.5 65.25 119.813 64.125 79.313 58.5 83.813"
+                ></polygon>
+                <polygon
+                  fill="#D87C30"
+                  points="46.125 101.25 92.25 102.375 87.188 126 65.25 120.375"
+                ></polygon>
+                <polygon
+                  fill="#EA8D3A"
+                  points="46.125 101.813 65.25 119.813 65.25 137.813"
+                ></polygon>
+                <polygon
+                  fill="#F89D35"
+                  points="65.25 120.375 87.75 126 95.063 150.188 90 153 65.25 138.375"
+                ></polygon>
+                <polygon
+                  fill="#EB8F35"
+                  points="65.25 138.375 60.75 173.25 90 153"
+                ></polygon>
+                <polygon
+                  fill="#EA8E3A"
+                  points="92.25 102.375 95.063 150.188 86.625 125.719"
+                ></polygon>
+                <polygon
+                  fill="#D87C30"
+                  points="39.375 138.938 65.25 138.375 60.75 173.25"
+                ></polygon>
+                <polygon
+                  fill="#EB8F35"
+                  points="12.938 188.438 60.75 173.25 39.375 138.938 .563 141.75"
+                ></polygon>
+                <polygon
+                  fill="#E8821E"
+                  points="88.875 58.5 64.688 78.75 46.125 101.25 92.25 102.938"
+                ></polygon>
+                <polygon
+                  fill="#393939"
+                  points="70.313 112.5 64.125 125.438 86.063 119.813"
+                  transform="matrix(-1 0 0 1 150.188 0)"
+                ></polygon>
+                <polygon
+                  fill="#E88F35"
+                  points="12.375 .563 88.875 58.5 75.938 27"
+                ></polygon>
+                <path
+                  fill="#8E5A30"
+                  d="M12.3750002,0.562500008 L2.25000003,31.5000005 L7.87500012,65.250001 L3.93750006,67.500001 L9.56250014,72.5625 L5.06250008,76.5000011 L11.25,82.1250012 L7.31250011,85.5000013 L16.3125002,96.7500014 L58.5000009,83.8125012 C79.1250012,67.3125004 89.2500013,58.8750003 88.8750013,58.5000009 C88.5000013,58.1250009 63.0000009,38.8125006 12.3750002,0.562500008 Z"
+                ></path>
+              </g>
+            </g>
+          </svg>
         </div>
         <div className="column">
-          {connectedAddress && (
-            <p>Connected Address: {connectedAddress}</p>
-          )}
+          {connectedAddress && <p>Connected Address: {connectedAddress}</p>}
         </div>
       </div>
       {/* Viewing of Members Address, Name and Task */}
       <div className="row">
         <div className="column">
-        <h2>Team Members</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Address</th>
-              <th>Name</th>
-              <th>Total Tasks Assigned</th>
-              <th>Total Tasks Completed</th>
-              <th>Active</th>
-            </tr>
-          </thead>
-          <tbody>
-            {teamMembers.map((member, index) => (
-              <tr key={index}>
-                <td>{member.walletAddress}</td>
-                <td>{member.name}</td>
-                <td>{member.totalTasksAssigned}</td>
-                <td>{member.totalTasksCompleted}</td>
-                <td>{member.isActive ? "🟢" : "🔴"}</td>
+          <h2>Team Members</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Address</th>
+                <th>Name</th>
+                <th>Total Tasks Assigned</th>
+                <th>Total Tasks Completed</th>
+                <th>Active</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {teamMembers.map((member, index) => (
+                <tr key={index}>
+                  <td>{member.walletAddress}</td>
+                  <td>{member.name}</td>
+                  <td>{member.totalTasksAssigned}</td>
+                  <td>{member.totalTasksCompleted}</td>
+                  <td>{member.isActive ? "🟢" : "🔴"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
         {/* Adding New Team Member */}
         <div className="column">
@@ -209,17 +342,25 @@ const ContractTeamManagement = () => {
         <div className="column">
           <h2>Assign Task</h2>
           <form onSubmit={handleAssignTask}>
-              <label>
-                  Team Member Address:
-                  <input type="text" value={teamMemberAddress} onChange={(e) => setTeamMemberAddress(e.target.value)} />
-              </label>
-              <br />
-              <label>
-                  Task Description:
-                  <input type="text" value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} />
-              </label>
-              <br />
-              <button type="submit">Assign Task</button>
+            <label>
+              Team Member Address:
+              <input
+                type="text"
+                value={teamMemberAddress}
+                onChange={(e) => setTeamMemberAddress(e.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Task Description:
+              <input
+                type="text"
+                value={taskDescription}
+                onChange={(e) => setTaskDescription(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="submit">Assign Task</button>
           </form>
         </div>
         {/* Task Completion */}
@@ -227,8 +368,12 @@ const ContractTeamManagement = () => {
           <h2>Complete Task</h2>
           <form onSubmit={handleCompleteTask}>
             <label>
-                Team Member Address:
-                <input type="text" value={teamMemberAddress} onChange={(e) => setTeamMemberAddress(e.target.value)} />
+              Team Member Address:
+              <input
+                type="text"
+                value={teamMemberAddress}
+                onChange={(e) => setTeamMemberAddress(e.target.value)}
+              />
             </label>
             <br />
             <button type="submit">Complete Task</button>
@@ -238,37 +383,45 @@ const ContractTeamManagement = () => {
       <div className="row">
         {/* Deactivating Team Member */}
         <div className="column">
-        <h2>Deactivate Team Member</h2>
-        <form onSubmit={handleDeactivateTeamMember}>
-          <label>
-            Team Member Address:
-            <input
-              type="text"
-              name="teamMember"
-              value={teamMemberAddress}
-              onChange={(e) => setTeamMemberAddress(e.target.value)}
-            />
-          </label>
-          <br />
-          <button type="submit">Deactivate Team Member</button>
-        </form>
-      </div>
+          <h2>Deactivate Team Member</h2>
+          <form onSubmit={handleDeactivateTeamMember}>
+            <label>
+              Team Member Address:
+              <input
+                type="text"
+                name="teamMember"
+                value={teamMemberAddress}
+                onChange={(e) => setTeamMemberAddress(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="submit">Deactivate Team Member</button>
+          </form>
+        </div>
         {/* Releasing Payment */}
         <div className="column">
           <h2>Release Payment</h2>
           <form onSubmit={handleReleasePayment}>
             <label>
-                Team Member Address:
-                <input type="text" value={teamMemberAddress} onChange={(e) => setTeamMemberAddress(e.target.value)} />
+              Team Member Address:
+              <input
+                type="text"
+                value={teamMemberAddress}
+                onChange={(e) => setTeamMemberAddress(e.target.value)}
+              />
             </label>
             <br />
             <label>
-                Amount:
-                <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
+              Amount:
+              <input
+                type="text"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
             </label>
             <br />
             <button type="submit">Release Payment</button>
-        </form>
+          </form>
         </div>
       </div>
     </div>
